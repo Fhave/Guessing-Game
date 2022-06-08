47050d77afe87fb0991e46d2841f2e65f0f21bdf
Fhave <68151313+Fhave@users.noreply.github.com>
2022-06-08 22:22:38 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+var userName = prompt('Username: ');
+console.log("Hello User, " + userName+"\n");
+
+let level = 1;
+let score = 0;
+
+function guess(range) {
+  console.log("Stage " + level)
+  var random = Math.floor(Math.random() *     (parseInt(range+1))) + 1;
+  
+  var number = parseInt(prompt('Guess a number from 1 to '+(range+1)+': '));
+  
+  if(number==random){
+    console.log("Correct")
+    range += 1;
+    level += 1;
+    score += 5;
+    console.log("Score: " +score+"\n")
+    guess(range)
+  }else{
+    console.log("Wrong");
+    var finalScore = score;
+    console.log("\n"+"Your Final Score: "+finalScore);
+  }
+}
+
+guess(1);
